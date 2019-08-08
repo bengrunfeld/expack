adf13675bdd99e58aee67d04dc4fb176954c1ef1
Kawika Heftel <kawika@luckykatworld.com>
2019-08-08 11:22:12 -0600

    chore: npm audit fix and linter error

--- src/js/logger.js
+++ src/js/logger.js
@@ -1,3 +1,4 @@
-const logMessage = msg => console.log(msg)
+// eslint-disable-next-line no-console
+const logMessage = msg => console.log(msg) // eslint-disable-line no-undef
 
 export default logMessage
